976fe3e007aeb0b4888dd1281f30beb759263040
Michael Bang <michael@katnegermis.dk>
2016-12-30 10:32:26 +0100

    Update bang_st{art,op} scripts, add spark-defaults.conf.

--- engine/bang_start.sh
+++ engine/bang_start.sh
@@ -1,17 +1,27 @@
+export SPARK_MASTER_IP=$(ifconfig | grep enp1s0f1 -A 1 |  grep inet | tr -s ' ' | cut -d' ' -f3)
 
-export SPARK_MASTER_IP=127.0.0.1
-export SPARK_MASTER_PORT=7077
+if [ "$SPARK_MASTER_PORT" = "" ]; then
+    export SPARK_MASTER_PORT=7077
+fi
+
 
 # Start master
+echo "Starting spark master on $SPARK_MASTER_IP:$SPARK_MASTER_PORT"
 sbin/start-master.sh
 
 # Start slave
 if [ "$SPARK_SLAVE_CORES" = "" ]; then
-  export SPARK_SLAVE_CORES=4
+  export SPARK_SLAVE_CORES=32
 fi
 
 if [ "$SPARK_SLAVE_MEMORY" = "" ]; then
-  export SPARK_SLAVE_MEMORY=11G
+  export SPARK_SLAVE_MEMORY=80g
 fi
 
-sbin/start-slave.sh -c $SPARK_SLAVE_CORES -m $SPARK_SLAVE_MEMORY "spark://$SPARK_MASTER_IP:$SPARK_MASTER_PORT"
+export SPARK_HOME=/home/tqg432/simba/Simba/engine/
+
+CMD=$(echo "cd . && cd $SPARK_HOME; pwd; sbin/start-slave.sh -c $SPARK_SLAVE_CORES -m $SPARK_SLAVE_MEMORY \"spark://$SPARK_MASTER_IP:$SPARK_MASTER_PORT\"")
+ssh gpu1 /bin/bash -c "$CMD"
+ssh gpu2 /bin/bash -c "$CMD"
+ssh gpu3 /bin/bash -c "$CMD"
+ssh gpu4 /bin/bash -c "$CMD"
--- engine/bang_stop.sh
+++ engine/bang_stop.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+export SPARK_HOME=/home/tqg432/simba/Simba/engine
+
 ./sbin/stop-master.sh
-./sbin/stop-slave.sh
+
+CMD=$(echo "cd . && cd $SPARK_HOME; \"sbin/stop-slave.sh\"")
+ssh gpu1 /bin/bash -c "$CMD"
+ssh gpu2 /bin/bash -c "$CMD"
+ssh gpu3 /bin/bash -c "$CMD"
+ssh gpu4 /bin/bash -c "$CMD"
 
